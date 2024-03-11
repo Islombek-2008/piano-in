1b889d81c2f4d72c3fbb01f909779bfc37e03aac
Islombek-2008 <playerislombek@gmail.com>
2024-03-11 17:55:21 +0500

    piano

---
+++ app.js
@@ -0,0 +1,35 @@
+let doAudio = new Audio();
+doAudio.src = "./music/do.mp3";
+
+let reAudio = new Audio();
+reAudio.src = "./music/re.mp3";
+
+let miAudio = new Audio();
+miAudio.src = "./music/mi.mp3";
+
+let faAudio = new Audio();
+faAudio.src = "./music/fa.mp3";
+
+let solAudio = new Audio();
+solAudio.src = "./music/sol.mp3";
+
+let lyaAudio = new Audio();
+lyaAudio.src = "./music/lya.mp3";
+
+let siAudio = new Audio();
+siAudio.src = "./music/si.mp3";
+
+let qora1 = new Audio();
+qora1.src = "./music/qora1.mp3";
+
+let qora2 = new Audio();
+qora2.src = "./music/qora2.mp3";
+
+let qora3 = new Audio();
+qora3.src = "./music/qora3.mp3";
+
+let qora4 = new Audio();
+qora4.src = "./music/qora4.mp3";
+
+let qora5 = new Audio();
+qora5.src = "./music/qora5.mp3";
